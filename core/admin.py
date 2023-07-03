b7e19bf034ea8c5dbe3c7bc26026e0190ebf119b
cristianvitor01 <cristianvitoralves@gmail.com>
2023-07-03 14:33:46 -0300

    register admin models

--- core/admin.py
+++ core/admin.py
@@ -1,3 +1,53 @@
 from django.contrib import admin
+from .models import Pizza, Cliente, EnderecoEntrega, Pedido, Item, Pagamento, PedidoStatus, Entrega, FeedBack
 
-# Register your models here.
+
+@admin.register(Pizza)
+class PizzaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'tipo', 'tamanho', 'ingredientes', 'preco']
+    list_filter = ['tipo', 'tamanho']
+
+
+@admin.register(Cliente)
+class ClienteAdmin(admin.ModelAdmin):
+    list_display = ['id', 'nome', 'endereco', 'telefone', 'email']
+    search_fields = ['nome', 'email']
+
+
+@admin.register(EnderecoEntrega)
+class EnderecoEntregaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cliente', 'endereco']
+
+
+@admin.register(Pedido)
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'cliente', 'endereco_entrega', 'data_pedido', 'valor_total']
+    list_filter = ['data_pedido']
+    date_hierarchy = 'data_pedido'
+
+
+@admin.register(Item)
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ['id', 'pedido', 'pizza', 'ingrediente_adicional', 'preco_item']
+    list_filter = ['pizza']
+
+
+@admin.register(Pagamento)
+class PagamentoAdmin(admin.ModelAdmin):
+    list_display = ['id', 'pedido', 'total_pagamento', 'metodo_pagamento']
+
+
+@admin.register(PedidoStatus)
+class PedidoStatusAdmin(admin.ModelAdmin):
+    list_display = ['id', 'pedido', 'status_atual', 'tempo_de_entrega']
+
+
+@admin.register(Entrega)
+class EntregaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'pedido', 'entrega_status']
+
+
+@admin.register(FeedBack)
+class EntregaAdmin(admin.ModelAdmin):
+    list_display = ['id', 'pedido', 'avaliacao', 'comentario']
+
